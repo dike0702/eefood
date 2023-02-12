d94040a15221e7aeb159deb9aed1521511b4cfb2
dike0702 <daiki.morita@smail.astate.edu>
2023-02-13 07:48:14 +0900

    daiki

--- eefood/application/urls.py
+++ eefood/application/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, Review
 
 urlpatterns = [
     path('', IndexView.as_view()),
-    # path('review/', Review.as_view()),
+    path('review/', Review.as_view()),
 ]
--- eefood/application/views.py
+++ eefood/application/views.py
@@ -2,3 +2,11 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = "daiki"
+        return ctxt
+
+class Review(TemplateView):
+    template_name = "review.html"
