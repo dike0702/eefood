9b3b17acdd544709cd2fe18d03a3831966864c8d
dike0702 <daiki.morita@smail.astate.edu>
2023-03-04 11:20:17 +0900

    profile modify

--- eefood/accounts/forms.py
+++ eefood/accounts/forms.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
@@ -11,3 +13,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class UserChangeForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["email"]
+    
+    def __init__(self, email=None, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+        if email:
+            self.fields['email'].widget.attrs['value'] = email
+    
+    def update(self, user):
+        user.email      = self.cleaned_data['email']
+        user.save()
--- eefood/accounts/views.py
+++ eefood/accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from django.views import View
 from django.http.response import HttpResponseRedirect
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, SignUpForm, UserChangeForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin   # ログインを強制させる
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 from django.urls import reverse_lazy
 
 class ProfileView(View):
@@ -29,3 +31,18 @@ class SignUpView(CreateView):
         login(self.request, user)
         self.object = user
         return HttpResponseRedirect(self.get_success_url())
+
+class UserChangeView(LoginRequiredMixin, FormView):
+    template_name = 'registration/change.html'
+    form_class = UserChangeForm
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        form.update(user=self.request.user)
+        return super().form_valid(form)
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            'email' : self.request.user.email,
+        })
+        return kwargs
