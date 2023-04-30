a202ae13abe972c8bd52a6d08a72834c9a5d001b
dike0702 <98758663+dike0702@users.noreply.github.com>
2023-04-30 10:27:15 +0900

    Delete eefood directory

--- eefood/application/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Restaurants, Review, Reservation
-
-admin.site.register(Restaurants)
-admin.site.register(Reservation)
-admin.site.register(Review)
